50bb2bba95d09b1a86387cbd6e5614aff3b6e66a
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-17 02:49:37 +0400

    user profile menu finish

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -9,36 +9,36 @@ import { useToast } from "@/hooks/use-toast";
 interface UserAuthForm extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthForm> = ({ className, ...props }) => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const { toast } = useToast();
-    const loginWithGoogle = async () => {
-        setIsLoading(true);
-        try {
-            await signIn("google");
-        } catch (error) {
-            toast({
-                title: "There was a problem.",
-                description: "There was an error logging in with Google",
-                variant: "destructive",
-            });
-        } finally {
-            setIsLoading(false);
-        }
-    };
-    return (
-        <div className={cn("flex justify-center", className)} {...props}>
-            <Button
-                onClick={loginWithGoogle}
-                isLoading={isLoading}
-                size="sm"
-                className="w-full "
-            >
-                {" "}
-                {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
-                Google
-            </Button>
-        </div>
-    );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full "
+      >
+        {" "}
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
 };
 
 export default UserAuthForm;
