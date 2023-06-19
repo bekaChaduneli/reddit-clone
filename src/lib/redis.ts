a9a0f034561ec3e65e16cf9f4d452de4b2fd94d7
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-19 20:56:12 +0400

    fix lints

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,4 +1,4 @@
-import { Redis } from "@upstash/redis";
+import { Redis } from "@upstash/redis/nodejs";
 export const redis = new Redis({
     url: process.env.REDIS_URL!,
     token: process.env.REDIS_SECRET!,
