d9629484155e1589bef15f981928f82bacb69f08
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-19 16:39:45 +0400

    post detail page finish

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,9 +1,15 @@
+import EditorOutput from "@/components/EditorOutput";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
 import { CachedPayload } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
+import { ArrowBigDown } from "lucide-react";
+import { ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 interface PageProps {
     params: {
         postId: string;
@@ -28,8 +34,55 @@ const page = async ({ params }: PageProps) => {
             },
         });
     }
-    if(!post && !cachedPost) return notFound();
-    return <div>page</div>;
+    if (!post && !cachedPost) return notFound();
+    return (
+        <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between ">
+            <Suspense fallback={<PostVoteShell />}>
+                <PostVoteServer
+                    postId={post?.id ?? cachedPost.id}
+                    getData={async () => {
+                        return await db.post.findUnique({
+                            where: {
+                                id: params.postId,
+                            },
+                            include: {
+                                votes: true,
+                            },
+                        });
+                    }}
+                />
+            </Suspense>
+
+            <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+                <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+                    Posted by u/
+                    {post?.author.username ?? cachedPost.authorUsername}{" "}
+                    {formatTimeToNow(
+                        new Date(post?.createdAt ?? cachedPost.createdAt)
+                    )}
+                </p>
+                <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+                    {post?.title ?? cachedPost.title}
+                </h1>
+                <EditorOutput content={post?.content ?? cachedPost.content} />
+            </div>
+        </div>
+    );
 };
+function PostVoteShell() {
+    return (
+        <div className="flex items-center flex-col pr-6 w-20">
+            <div className={buttonVariants({ variant: "ghost" })}>
+                <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+            </div>
+            <div className="text-center py-2 font-medium text-sm text-zinc-900">
+                <Loader2 className="h-5 w-5 animate-spin" />
+            </div>
+            <div className={buttonVariants({ variant: "ghost" })}>
+                <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+            </div>
+        </div>
+    );
+}
 
 export default page;
