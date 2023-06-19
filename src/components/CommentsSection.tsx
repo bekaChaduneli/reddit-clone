1b0821106a74e1ca73aeab5f1cd33131b44c3b16
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-19 18:38:11 +0400

    comments reply finish

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,16 +1,31 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
-import PostComment from "./PostComment";
+import { Comment, CommentVote, User } from "@prisma/client";
 import CreateComment from "./CreateComment";
+import PostComment from "./comments/PostComment";
+
+type ExtendedComment = Comment & {
+    votes: CommentVote[];
+    author: User;
+    replies: ReplyComment[];
+};
+
+type ReplyComment = Comment & {
+    votes: CommentVote[];
+    author: User;
+};
 
 interface CommentsSectionProps {
     postId: string;
+    comments: ExtendedComment[];
 }
+
 const CommentsSection = async ({ postId }: CommentsSectionProps) => {
     const session = await getAuthSession();
+
     const comments = await db.comment.findMany({
         where: {
-            postId,
+            postId: postId,
             replyToId: null,
         },
         include: {
@@ -24,10 +39,13 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
             },
         },
     });
+
     return (
         <div className="flex flex-col gap-y-4 mt-4">
-            <hr className="w-full h-px py-6" />
+            <hr className="w-full h-px my-6" />
+
             <CreateComment postId={postId} />
+
             <div className="flex flex-col gap-y-6 mt-4">
                 {comments
                     .filter((comment) => !comment.replyToId)
@@ -42,6 +60,7 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
                         const topLevelCommentVote = topLevelComment.votes.find(
                             (vote) => vote.userId === session?.user.id
                         );
+
                         return (
                             <div
                                 key={topLevelComment.id}
@@ -55,6 +74,40 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
                                         postId={postId}
                                     />
                                 </div>
+                                {topLevelComment.replies
+                                    .sort(
+                                        (a, b) =>
+                                            b.votes.length - a.votes.length
+                                    )
+                                    .map((reply) => {
+                                        const replyVotesAmt =
+                                            reply.votes.reduce((acc, vote) => {
+                                                if (vote.type === "UP")
+                                                    return acc + 1;
+                                                if (vote.type === "DOWN")
+                                                    return acc - 1;
+                                                return acc;
+                                            }, 0);
+
+                                        const replyVote = reply.votes.find(
+                                            (vote) =>
+                                                vote.userId === session?.user.id
+                                        );
+
+                                        return (
+                                            <div
+                                                key={reply.id}
+                                                className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                                            >
+                                                <PostComment
+                                                    comment={reply}
+                                                    currentVote={replyVote}
+                                                    votesAmt={replyVotesAmt}
+                                                    postId={postId}
+                                                />
+                                            </div>
+                                        );
+                                    })}
                             </div>
                         );
                     })}
