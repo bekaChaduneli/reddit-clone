66c31d7bde7a9e723aa2ed9ee4548725f434095e
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-20 14:47:17 +0400

    fix database errors

--- src/components/homepage/CustomFeed.tsx
+++ src/components/homepage/CustomFeed.tsx
@@ -1,45 +1,44 @@
-import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
-import { getAuthSession } from '@/lib/auth'
-import { db } from '@/lib/db'
-import PostFeed from '../PostFeed'
-import { notFound } from 'next/navigation'
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import PostFeed from "../PostFeed";
+import { notFound } from "next/navigation";
 
 const CustomFeed = async () => {
-  const session = await getAuthSession()
+    const session = await getAuthSession();
 
-  // only rendered if session exists, so this will not happen
-  if (!session) return notFound()
+    if (!session) return notFound();
 
-  const followedCommunities = await db.subscription.findMany({
-    where: {
-      userId: session.user.id,
-    },
-    include: {
-      subreddit: true,
-    },
-  })
-
-  const posts = await db.post.findMany({
-    where: {
-      subreddit: {
-        name: {
-          in: followedCommunities.map((sub) => sub.subreddit.name),
+    const followedCommunities = await db.subscription.findMany({
+        where: {
+            userId: session?.user.id,
         },
-      },
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-    include: {
-      votes: true,
-      author: true,
-      comments: true,
-      subreddit: true,
-    },
-    take: INFINITE_SCROLL_PAGINATION_RESULTS,
-  })
+        include: {
+            subreddit: true,
+        },
+    });
 
-  return <PostFeed initialPosts={posts} />
-}
+    const posts = await db.post.findMany({
+        where: {
+            subreddit: {
+                name: {
+                    in: followedCommunities.map((sub) => sub.subreddit.name),
+                },
+            },
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            votes: true,
+            author: true,
+            comments: true,
+            subreddit: true,
+        },
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+    });
 
-export default CustomFeed
+    return <PostFeed initialPosts={posts} />;
+};
+
+export default CustomFeed;
