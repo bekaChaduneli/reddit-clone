0de2e3d63b1fc05a1bea73e0afe4c9acb6a4b906
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-20 13:14:23 +0400

    change lints

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,21 +1,15 @@
-'use client'
+"use client";
 
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { SessionProvider } from 'next-auth/react'
-import { FC, ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
-interface LayoutProps {
-  children: ReactNode
-}
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider>{children}</SessionProvider>
+        </QueryClientProvider>
+    );
+};
 
-const Providers: FC<LayoutProps> = ({ children }) => {
-  const queryClient = new QueryClient()
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider>{children}</SessionProvider>
-    </QueryClientProvider>
-  )
-}
-
-export default Providers
+export default Providers;
